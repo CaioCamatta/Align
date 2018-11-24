2203afd21ed1e6a827beb6aad33fdd86b46bc882
Caio Camatta <caiocamattacoelho@hotmail.com>
2018-11-24 14:14:58 -0500

    Added head proximity and tilt to the algorithm.

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,6 @@
 /* This files receives the image data as input and tells you whether your posture is good or not*/
 // Variable to store calibration data
-var calibrationData = [100];
+var calibrationData = [100, 250, -6];
 
 // Main function to evaluate posture
 module.exports.posture = (imageData) => {
@@ -10,10 +10,38 @@ module.exports.posture = (imageData) => {
   pan = imageData.pan;
   tilt =imageData.tilt;
 
-  // Compare current image with calibration data
+  // Check head position
   if(outer_vertices[0].y < calibrationData[0]){
-    return true;
+    console.log('Head position: good')
   } else {
+    console.log('Head too low!')
+    return false;
+  };
+
+  // Check head proximity
+  current_box_size = inner_vertices[1].x - inner_vertices[0].x;
+  calibration_box_size = calibrationData[1];
+
+  // Compare the size of the calibration box to the current box size
+  if(current_box_size < calibration_box_size){
+    console.log('Head proximity: good')
+  } else {
+    console.log('Head too close!')
+    return false;
+  };
+
+  // Check head tilt
+  if(tilt > calibrationData[2]){
+    console.log('Head tilt: good')
+  } else {
+    console.log('Head too tilted!')
     return false;
   };
 };
+
+/*
+Types of bad posture:
+  head position
+  head proximity
+  tilt
+*/
